4b1712996bbf9a117a77393058725e0c513060c1
Carlos Rodriguez <carlosrodriguez@Carlos-MacBook-Pro.local>
2015-10-06 22:46:15 +0200

    Added solution to problem 6.

--- problem6.hs
+++ problem6.hs
@@ -3,5 +3,3 @@ problem6 =
   let n = 100 in floor $ squareOfSum n - sumOfSquares n
   where squareOfSum n = (n * (n + 1) / 2) ^ 2
         sumOfSquares n = n * (n + 1) * (2 * n + 1) / 6
-        
-  
