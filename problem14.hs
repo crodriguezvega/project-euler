3b9b4affd25828504375e5cc0e867815296d4d1e
Carlos Rodriguez <carlosrodriguez@Carlos-MacBook-Pro.local>
2015-10-22 21:59:23 +0200

    Memoization was not working.

--- problem14.hs
+++ problem14.hs
@@ -1,11 +1,10 @@
 import Data.List
 import Data.Ord
 
-memoizedCollatzLength :: Int -> Int
-memoizedCollatzLength = (map collatzLength [0..] !!)
-  where collatzLength 1 = 1
-        collatzLength number = 1 + memoizedCollatzLength x
-          where x = if even number then quot number 2 else 3 * number + 1
+collatzLength :: Integer -> Integer
+collatzLength 1 = 1
+collatzLength number = 1 + collatzLength x
+  where x = if even number then quot number 2 else 3 * number + 1
 
-problem14 :: Int
-problem14 = fst $ maximumBy (comparing snd) $ map (\n -> (n, memoizedCollatzLength n)) [1..1000000]
+problem14 :: Integer
+problem14 = fst $ maximumBy (comparing snd) $ map (\n -> (n, collatzLength n)) [1..1000000]
