3033ffa6a662f12bcd6a8ed9644634da963c638e
Carlos Rodriguez <carlosrodriguez@Carlos-MacBook-Pro.local>
2015-11-12 06:51:53 +0100

    Improved solution to problem 8.

--- problem_008.hs
+++ problem_008.hs
@@ -5,9 +5,9 @@ toIntegers [] = []
 toIntegers (x:xs) = digitToInt x : toIntegers xs
 
 windowProduct :: (Num a) => [a] -> [a]
-windowProduct all@(x1:x2:x3:x4:x5:x6:x7:x8:x9:x10:x11:x12:x13:xs) =
-  let product = x1*x2*x3*x4*x5*x6*x7*x8*x9*x10*x11*x12*x13 in product : windowProduct (tail all)
-windowProduct _ = [] 
+windowProduct xs
+  | length xs >= 13 = ( foldr (*) 1 $ take 13 xs) : windowProduct (tail xs)
+  | otherwise       = []
 
 readLines :: FilePath -> IO [String]
 readLines = fmap lines . readFile
