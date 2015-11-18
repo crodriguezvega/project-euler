a5e64b24787e372d1ad5c97da30cb41506d658f9
Carlos Rodriguez <carlosrodriguez@Carlos-MacBook-Pro.local>
2015-11-18 21:59:15 +0100

    Improved solution to problem 5.

--- problem_005.hs
+++ problem_005.hs
@@ -1,5 +1,2 @@
 problem5 :: Integer
-problem5 =
-  let evenlyDivisible = [x | x <- [1..232792565], isEvenlyDivisible x]
-  in head evenlyDivisible
-  where isEvenlyDivisible n = foldl (\acc x -> n `mod` x == 0 && acc) True [1..20]
+problem5 = foldr1 lcm [1..20]
