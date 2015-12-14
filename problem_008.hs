58440382a65dcce02aca93bdc96c7964bfbd0b1f
Carlos Rodriguez <carlosrodriguez@Carlos-MacBook-Pro.local>
2015-12-14 23:12:56 +0100

    Updated solution to problem 8.

--- problem_008.hs
+++ problem_008.hs
@@ -1,16 +1,16 @@
 import Data.Char
 
-toIntegers :: String -> [Int]
-toIntegers = map digitToInt
-
 windowProduct :: (Num a) => [a] -> [a]
 windowProduct xs
   | length xs >= 13 = (foldr (*) 1 $ take 13 xs) : windowProduct (tail xs)
   | otherwise       = []
+
+toIntegers :: String -> [Int]
+toIntegers = map digitToInt
 
 readLines :: FilePath -> IO [String]
 readLines = fmap lines . readFile
 
 problem8 = do
   content <- readLines "files/problem_008.txt"
-  return (maximum $ windowProduct $ toIntegers $ concat content)
+  return $ maximum $ windowProduct $ toIntegers $ concat content
