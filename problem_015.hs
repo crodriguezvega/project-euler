c3255d45f5a706905af960ec1053f1fa8844e216
Carlos Rodriguez <carlosrodriguez@Carlos-MacBook-Pro.local>
2015-12-28 00:30:39 +0100

    Added solution to problem 15.

---
+++ problem_015.hs
@@ -0,0 +1,9 @@
+binomialCoefficient :: Integer -> Integer -> Integer
+binomialCoefficient 0 _ = 1
+binomialCoefficient _ 0 = 1
+binomialCoefficient n k = numerator `div` denominator
+                          where numerator = product [n - k + 1..n]
+                                denominator = product [1..k]
+
+problem15 :: Integer
+problem15 = binomialCoefficient 40 20
