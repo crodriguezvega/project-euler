77b67080fc92a4009b6958e47639bf826fef7d64
Carlos Rodriguez <crodveg@yahoo.es>
2016-01-23 18:29:20 +0100

    Solution to problem 67. :Q

---
+++ problem_067.hs
@@ -0,0 +1,18 @@
+problem67 = do
+  content <- readLines "files/problem_067.txt"
+  return $ head $ maxSumPath $ reverse $ map readNumbers content
+
+readLines :: FilePath -> IO [String]
+readLines = fmap lines . readFile
+
+readNumbers :: String -> [Int]
+readNumbers = map read . words
+
+maxSumPath :: [[Int]] -> [Int]
+maxSumPath (x:[])     = x
+maxSumPath (x0:x1:xs) = maxSumPath $ (maxSum x1 x0):xs
+
+maxSum :: [Int] -> [Int] -> [Int]
+maxSum [] _              = []
+maxSum (x:xs) (y0:y1:ys) = x + max : maxSum xs (y1:ys)
+                           where max = if y0 >= y1 then y0 else y1
