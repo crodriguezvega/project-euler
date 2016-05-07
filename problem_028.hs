c342c17aedde4701edf19394ae437bf97bb93785
Carlos Rodriguez <crodveg@yahoo.es>
2016-05-07 17:26:15 +0200

    Added solution to problem 28.

---
+++ problem_028.hs
@@ -0,0 +1,8 @@
+problem28 :: Int
+problem28 = 1 + spiralDiagonals 1 [2, 4 .. 1000]
+
+spiralDiagonals n []             = 0
+spiralDiagonals n (increment:xs) = total + spiralDiagonals next xs
+  where next = head corners
+        total = sum corners
+        corners = map (\x -> x * increment + n) [4, 3 .. 1]
