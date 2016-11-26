5e32851141b2e0db1e9328eb2d2193bff213b3fe
Carlos Rodriguez <crodveg@yahoo.es>
2016-11-26 11:59:38 +0100

    Solution to problem 39.

--- problem_009.hs
+++ problem_009.hs
@@ -17,8 +17,8 @@ problem9 =
 pythagoreanTriplet :: (Integral a) => (a, a) -> (a, a, a)
 pythagoreanTriplet (m, n) =
   let a = m^2 - n^2
-      b = (2 * m * n);
-      c = (m^2 + n^2);
+      b = 2 * m * n;
+      c = m^2 + n^2;
   in (a, b, c)
 
 productTriplet :: (Integral a) => (a, a, a) -> a
---
+++ problem_039.hs
@@ -0,0 +1,54 @@
+{-
+  We need to generate first the list of primitive Pythagorean triplets:
+
+  http://mathforum.org/library/drmath/view/55811.html
+  http://aleph0.clarku.edu/~djoyce/numbers/pyth.pdf
+
+  Thus the formulas that give all Pythagorean triples are these:
+
+   a = d*(m^2 - n^2),
+   b = 2*d*m*n,
+   c = d*(m^2 + n^2),
+
+  where d is any positive integer, m > n > 0 are integers of opposite 
+  parity and relatively prime.
+-}
+
+import Data.Ord
+import Data.List
+
+problem39 = head $ maximumBy (comparing (\x -> length x)) 
+                 $ group
+                 $ sort
+                 $ map sumTriplet
+                 $ concatMap generateMultipleTriplets
+                 $ concat
+                 $ takeWhile (\x -> length x > 0)
+                 $ map generatePrimitiveTriplets [2..]
+
+generateMultipleTriplets primitive = takeWhile condition $ map generateMultiple [1..]
+  where generateMultiple d = multiplePythagoreanTriplet primitive d
+
+generatePrimitiveTriplets m = takeWhile condition $ map primitivePythagoreanTriplet pairs
+  where pairs = [(m, n) | n <- generateN m]
+
+generateN m = filter (\x -> isCoprime x && isOppositeParity x) [start..m-1]
+  where isOppositeParity n = not $ isEven $ abs (m - n)
+        isCoprime n = gcd n m == 1
+        isEven x = x `mod` 2 == 0
+        start | isEven m  = 1
+              | otherwise = 2
+
+multiplePythagoreanTriplet (a, b, c) d = (d * a, d * b, d * c)
+
+primitivePythagoreanTriplet (m, n) =
+  let a = m^2 - n^2
+      b = 2 * m * n;
+      c = m^2 + n^2;
+  in (a, b, c)
+
+sumTriplet (a, b, c) = a + b + c
+
+targetSum = 1000
+
+condition triplet = sumTriplet triplet <= targetSum
