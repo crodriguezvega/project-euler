658cf058349e4e16e012399e876fcc6f6dc458ba
crodriguezvega <crodveg@yahoo.es>
2018-04-06 22:22:11 +0200

    Solution to problem 52.

---
+++ problem_052.hs
@@ -0,0 +1,19 @@
+import Data.List
+
+problem52 = head $ dropWhile predicate candidates
+
+predicate n = not $ condition n (products n)
+
+condition n [] = True
+condition n (x:xs) = sameDigits n x && condition n xs
+
+sameDigits n m = length a == length b && (a \\ b) == []
+  where a = toDigits n
+        b = toDigits m
+
+products n = [n * x | x <- [1..6]]
+
+toDigits 0 = []
+toDigits n = n `mod` 10 : toDigits (n `div` 10)
+
+candidates = [y | x <- [0..], let a = 10 * 10 ^ x, let b = 16 * 10 ^ x, y <- [a..b] ]
