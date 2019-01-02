e56ad1b9c58af762d086e819f40eaff9e2cdb983
crodriguezvega <crodveg@yahoo.es>
2019-01-02 20:31:49 +0100

    Added space.

--- problem_053.hs
+++ problem_053.hs
@@ -1,7 +1,7 @@
 import qualified Data.Map as Map
 
 problem53 :: Int
-problem53 = length $ filter (>1000000) $ [combinations n r| n <- [1..100], r <- [1..n]]
+problem53 = length $ filter (>1000000) $ [combinations n r | n <- [1..100], r <- [1..n]]
 
 combinations n r = factorial n `div` (factorial r * factorial (n - r))
 
