dea14ba9f65c51f39104a2b612bd52c10c6bf035
crodriguezvega <crodveg@yahoo.es>
2019-01-02 20:31:12 +0100

    Added solution to problem 53.

---
+++ problem_053.hs
@@ -0,0 +1,12 @@
+import qualified Data.Map as Map
+
+problem53 :: Int
+problem53 = length $ filter (>1000000) $ [combinations n r| n <- [1..100], r <- [1..n]]
+
+combinations n r = factorial n `div` (factorial r * factorial (n - r))
+
+factorial :: Integer -> Integer
+factorial n = factorials Map.! n
+
+factorials :: Map.Map Integer Integer
+factorials = Map.fromList $ zip [0..100] $ scanl (*) 1 [1..100]
