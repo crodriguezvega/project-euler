28ddb907d03864e77790b6fa5bd440d24e9612d6
crodriguezvega <crodveg@yahoo.es>
2019-01-02 20:34:07 +0100

    Added function signature.

--- problem_053.hs
+++ problem_053.hs
@@ -3,6 +3,7 @@ import qualified Data.Map as Map
 problem53 :: Int
 problem53 = length $ filter (>1000000) $ [combinations n r | n <- [1..100], r <- [1..n]]
 
+combinations :: Integer -> Integer -> Integer
 combinations n r = factorial n `div` (factorial r * factorial (n - r))
 
 factorial :: Integer -> Integer
